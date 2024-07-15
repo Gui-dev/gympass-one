2aa1f8d85fa2b80faf1b745c4847650fe7872188
Gui Silva <adrianon10@gmail.com>
2024-07-15 05:14:15 -0300

    test: it should not be able to register with same email twice

--- src/use-cases/__tests__/register.spec.ts
+++ src/use-cases/__tests__/register.spec.ts
@@ -1,6 +1,7 @@
 import { compare } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
 
+import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 
 import { RegisterUseCase } from '../register'
@@ -21,5 +22,26 @@ describe('User register', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const email = 'fake_email@email.com'
+
+    await registerUseCase.execute({
+      name: 'Fake Name',
+      email,
+      password: 'fake_password',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'Fake Name',
+        email,
+        password: 'fake_password',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
