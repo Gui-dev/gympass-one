9469cbdcc92cfbdd1652c7422c1c9f0eb84655ac
Gui Silva <adrianon10@gmail.com>
2024-07-15 05:04:32 -0300

    test: it should hash user password upon registration

--- src/use-cases/__tests__/register.spec.ts
+++ src/use-cases/__tests__/register.spec.ts
@@ -1,5 +1,25 @@
-import { expect, test } from 'vitest'
+import { compare } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
 
-test('sum', () => {
-  expect(2 + 2).toBe(4)
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+import { RegisterUseCase } from '../register'
+
+describe('User register', () => {
+  it('should hash user password upon registration', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Fake Name',
+      email: 'fake_email@email.com',
+      password: 'fake_password',
+    })
+    const isPasswordCorrectlyHashed = await compare(
+      'fake_password',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,3 +1,4 @@
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 
 import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
@@ -9,10 +10,18 @@ interface IRegisterRequest {
   password: string
 }
 
+interface IRegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  public async execute({ name, email, password }: IRegisterRequest) {
+  public async execute({
+    name,
+    email,
+    password,
+  }: IRegisterRequest): Promise<IRegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
 
     const userAlreadyExists = await this.usersRepository.findByEmail(email)
@@ -21,10 +30,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
