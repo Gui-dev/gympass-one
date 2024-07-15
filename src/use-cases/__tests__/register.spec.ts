465f7629b22734e1808f4da4281491dce8132bf7
Gui Silva <adrianon10@gmail.com>
2024-07-15 05:17:03 -0300

    test: it should be able to register

--- src/use-cases/__tests__/register.spec.ts
+++ src/use-cases/__tests__/register.spec.ts
@@ -7,6 +7,18 @@ import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user
 import { RegisterUseCase } from '../register'
 
 describe('User register', () => {
+  it('should hbe able to register', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'Fake Name',
+      email: 'fake_email@email.com',
+      password: 'fake_password',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
   it('should hash user password upon registration', async () => {
     const inMemoryUsersRepository = new InMemoryUsersRepository()
     const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
