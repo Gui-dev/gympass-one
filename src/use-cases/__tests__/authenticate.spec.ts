e61ff7a6ba94108114001575680ffa1b82bf54b0
Gui Silva <adrianon10@gmail.com>
2024-07-16 03:33:59 -0300

    test: it should be able to authenticate

---
+++ src/use-cases/__tests__/authenticate.spec.ts
@@ -0,0 +1,26 @@
+import { hash } from 'bcryptjs'
+import { describe, expect, it } from 'vitest'
+
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+import { AuthenticateUseCase } from '../authenticate'
+
+describe('Authenticate', () => {
+  it('should be able to authenticate', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(inMemoryUsersRepository) // sut => System under test
+
+    await inMemoryUsersRepository.create({
+      name: 'Fake Name',
+      email: 'fake_email@email.com',
+      password_hash: await hash('fake_password', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'fake_email@email.com',
+      password: 'fake_password',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+})
