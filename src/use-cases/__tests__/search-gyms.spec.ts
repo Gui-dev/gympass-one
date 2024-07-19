00ee0b58427b54a402729e13cb182c52687f7d91
Gui Silva <adrianon10@gmail.com>
2024-07-19 04:37:36 -0300

    test: it should be able to fetch paginated gym search

--- src/use-cases/__tests__/search-gyms.spec.ts
+++ src/use-cases/__tests__/search-gyms.spec.ts
@@ -33,4 +33,24 @@ describe('Search Gyms Use Case', () => {
     expect(gyms).toHaveLength(1)
     expect(gyms).toEqual([expect.objectContaining({ title: 'Fake Gym 2' })])
   })
+
+  it('should be able to fetch paginated gym search', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryGymsRepository.create({
+        title: `Fake Gym ${i}`,
+        description: `Fake description ${i}`,
+        phone: `Fake phone ${i}`,
+        latitude: -23.7833232,
+        longitude: -46.6801928,
+      })
+    }
+
+    const { gyms } = await sut.execute({ query: 'Fake', page: 2 })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: 'Fake Gym 21' }),
+      expect.objectContaining({ title: 'Fake Gym 22' }),
+    ])
+  })
 })
