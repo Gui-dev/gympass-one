296ee201850d079a89df6ba9206c36c32e66f067
Gui Silva <adrianon10@gmail.com>
2024-07-19 03:55:33 -0300

    test: it should be able to fetch paginated check in history

--- src/use-cases/__tests__/fetch-user-check-ins-history.spec.ts
+++ src/use-cases/__tests__/fetch-user-check-ins-history.spec.ts
@@ -23,12 +23,35 @@ describe('Fetch User Check Ins History', () => {
       gym_id: 'fake_gym_id_2',
     })
 
-    const { check_ins } = await sut.execute({ user_id: 'fake_user_id' })
+    const { check_ins } = await sut.execute({
+      user_id: 'fake_user_id',
+      page: 1,
+    })
 
     expect(check_ins).toHaveLength(2)
     expect(check_ins).toEqual([
       expect.objectContaining({ gym_id: 'fake_gym_id' }),
       expect.objectContaining({ gym_id: 'fake_gym_id_2' }),
+    ])
+  })
+
+  it('should be able to fetch paginated check in history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryCheckInsRepository.create({
+        user_id: 'fake_user_id',
+        gym_id: `fake_gym_id_${i}`,
+      })
+    }
+
+    const { check_ins } = await sut.execute({
+      user_id: 'fake_user_id',
+      page: 2,
+    })
+
+    expect(check_ins).toHaveLength(2)
+    expect(check_ins).toEqual([
+      expect.objectContaining({ gym_id: 'fake_gym_id_21' }),
+      expect.objectContaining({ gym_id: 'fake_gym_id_22' }),
     ])
   })
 })
