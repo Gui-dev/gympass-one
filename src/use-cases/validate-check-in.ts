59b6fe8a59aa4172750ffd6f0def1b0993da2d3f
Gui Silva <adrianon10@gmail.com>
2024-07-20 04:22:57 -0300

    feat: add check-in can only be validated up to 20 minutes after it is created

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,5 +1,7 @@
 import { CheckIn } from '@prisma/client'
+import dayjs from 'dayjs'
 
+import { LateCheckInValidationError } from '@/errors/late-check-in-validation-error'
 import { ResourceNotFoundError } from '@/errors/resource-not-found-error'
 import { ICheckInsRepository } from '@/repositories/contracts/check-ins-repository'
 
@@ -21,6 +23,16 @@ export class ValidateCheckInUseCase {
 
     if (!checkInExists) {
       throw new ResourceNotFoundError()
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkInExists.created_at,
+      'minutes',
+    )
+    const MAX_MINUTES = 20
+
+    if (distanceInMinutesFromCheckInCreation > MAX_MINUTES) {
+      throw new LateCheckInValidationError()
     }
 
     checkInExists.validated_at = new Date()
