bf5193769a5de4e6bb65fefb7ed899ff4fd036d9
Gui Silva <adrianon10@gmail.com>
2024-07-23 03:52:18 -0300

    feat: add implemented the methods findManyNearby and searchMany

--- src/repositories/prisma/gyms-repository.ts
+++ src/repositories/prisma/gyms-repository.ts
@@ -2,7 +2,10 @@ import { Gym, Prisma } from '@prisma/client'
 
 import { prisma } from '@/lib/prisma'
 
-import { IGymsRepository } from '../contracts/gyms-repository'
+import {
+  IFindManyNearbyParams,
+  IGymsRepository,
+} from '../contracts/gyms-repository'
 
 export class GymsRepository implements IGymsRepository {
   public async findById(id: string): Promise<Gym | null> {
@@ -13,6 +16,35 @@ export class GymsRepository implements IGymsRepository {
     })
 
     return gym
+  }
+
+  public async findManyNearby({
+    latitude,
+    longitude,
+  }: IFindManyNearbyParams): Promise<Gym[]> {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * 
+      cos( radians( latitude ) ) * 
+      cos( radians( longitude ) - radians(${longitude}) ) + 
+      sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+
+  public async searchMany(query: string, page: number): Promise<Gym[]> {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
   }
 
   public async create(data: Prisma.GymCreateInput): Promise<Gym> {
