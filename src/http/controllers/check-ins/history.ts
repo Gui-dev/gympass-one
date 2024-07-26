8819a323a9c3e16be8dc9728926278b011a29a1f
Gui Silva <adrianon10@gmail.com>
2024-07-26 04:21:46 -0300

    feat: add history check-ins controller

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeFetchUserCheckInsHistoryUseCase } from '@/use-cases/factories/make-fetch-user-check-ins-history-use-case'
+
+export const history = async (request: FastifyRequest, reply: FastifyReply) => {
+  const user_id = request.user.sub
+  const searchGymQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1),
+  })
+  const { page } = searchGymQuerySchema.parse(request.query)
+
+  const fetchUserCheckInsHistoryUseCase = makeFetchUserCheckInsHistoryUseCase()
+  const { check_ins } = await fetchUserCheckInsHistoryUseCase.execute({
+    user_id,
+    page,
+  })
+  return reply.status(201).send({ check_ins })
+}
