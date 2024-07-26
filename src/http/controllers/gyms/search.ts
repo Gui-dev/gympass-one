778768fb137dddeaba737ef87f6e1305520cba49
Gui Silva <adrianon10@gmail.com>
2024-07-26 03:48:02 -0300

    feat: add search gyms controller

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,19 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export const search = async (request: FastifyRequest, reply: FastifyReply) => {
+  const searchGymQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+  const { query, page } = searchGymQuerySchema.parse(request.body)
+
+  const searchGymUseCase = makeSearchGymsUseCase()
+  const { gyms } = await searchGymUseCase.execute({
+    query,
+    page,
+  })
+  return reply.status(201).send({ gyms })
+}
