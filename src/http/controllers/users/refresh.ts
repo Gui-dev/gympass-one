98e9f23706e0ca50dbc0553f5d5b8b398c86fe5b
Gui Silva <adrianon10@gmail.com>
2024-07-29 03:37:29 -0300

    feat: add refresh token controller

---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,33 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export const refresh = async (request: FastifyRequest, reply: FastifyReply) => {
+  await request.jwtVerify({ onlyCookie: true })
+
+  const token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+      },
+    },
+  )
+  const refresh_token = await reply.jwtSign(
+    {},
+    {
+      sign: {
+        sub: request.user.sub,
+        expiresIn: '7d',
+      },
+    },
+  )
+
+  return reply
+    .setCookie('refresh_token', refresh_token, {
+      path: '/',
+      secure: true, // HTTPS
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({ token })
+}
